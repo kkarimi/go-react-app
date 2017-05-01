8dbb84d1a5fb7531c9ab206a6718e050cf9f42fd
Kash Karimi <kash.karimi@gmail.com>
2017-05-01 14:41:40 +0100

    Add rethink db connection, use logs

--- backend/main.go
+++ backend/main.go
@@ -1,87 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/websocket"
-	"github.com/mitchellh/mapstructure"
+	r "github.com/dancannon/gorethink"
+	"log"
 	"net/http"
-	"time"
 )
 
-type Message struct {
-	Name string      `json:"name"`
-	Data interface{} `json:"data"`
-}
-
+//Channel struct
 type Channel struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
+	ID   string `json:"id" gorethink:"id,omitempty"`
+	Name string `json:"name" gorethink:"name"`
 }
 
-// handle cross-origin by func
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
+type User struct {
+	ID   string `gorethink:"id,omitempty"`
+	Name string `gorethink:"name"`
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: "chatserver",
+	})
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	router := NewRouter(session)
+	router.Handle("channel add", addChannel)
+	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	// hijack connection and switch protocol from http to websockets
-	socket, err := upgrader.Upgrade(w, r, nil)
-	// handle error from ?
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	for {
-		var inMessage Message
-		if err := socket.ReadJSON(&inMessage); err != nil {
-			fmt.Println(err)
-			break
-		}
-		fmt.Printf("%#v\n", inMessage)
-		switch inMessage.Name {
-		case "channel add":
-			err := addChannel(inMessage.Data)
-			if err != nil {
-				outMessage := Message{"error", err}
-				if err := socket.WriteJSON(outMessage); err != nil {
-					fmt.Println(err)
-					break
-				}
-			}
-		case "channel subscribe":
-			// use go routines to create a light thread
-			// so not to block app from other actions
-			go subscribeChannel(socket)
-		}
-	}
-}
-
-func addChannel(data interface{}) error {
-	var channel Channel
-	err := mapstructure.Decode(data, &channel)
-	if err != nil {
-		return err
-	}
-	channel.Id = "1"
-	// fmt.Printf("%#v\n", channel)
-	fmt.Println("added channel")
-	return nil
-}
-
-func subscribeChannel(socket *websocket.Conn) {
-	for {
-		time.Sleep(time.Second * 1)
-		message := Message{"channel add",
-			Channel{"1", "Some new channel"}}
-		socket.WriteJSON(message)
-		fmt.Println("send new channel")
-	}
 }

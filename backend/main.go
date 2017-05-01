fa9c29e85c082c9fbfcfa74b8a783b299071c853
Kash Karimi <kash.karimi@gmail.com>
2017-05-01 16:57:25 +0100

    clean up, add new handlers

--- backend/main.go
+++ backend/main.go
@@ -6,17 +6,6 @@ import (
 	"net/http"
 )
 
-//Channel struct
-type Channel struct {
-	ID   string `json:"id" gorethink:"id,omitempty"`
-	Name string `json:"name" gorethink:"name"`
-}
-
-type User struct {
-	ID   string `gorethink:"id,omitempty"`
-	Name string `gorethink:"name"`
-}
-
 func main() {
 	session, err := r.Connect(r.ConnectOpts{
 		Address:  "localhost:28015",
@@ -26,9 +15,18 @@ func main() {
 		log.Panic(err.Error())
 	}
 	router := NewRouter(session)
+
 	router.Handle("channel add", addChannel)
 	router.Handle("channel subscribe", subscribeChannel)
-	route.Handle("channel unsubscribe", unsubscribeChannel)
+	router.Handle("channel unsubscribe", unsubscribeChannel)
+
+	// User routes
+	// router.Handle("user edit", editUser)
+	// router.Handle("user subscribe", subscribeUser)
+	// router.Handle("user unsubscribe", unsubscribeIser)
+
+	// Message routes
+
 	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
 }
